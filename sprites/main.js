5f7995624081560e94cb5913d59f4b377b5313b0
nguyenHienTrang99 <115915656+nguyenHienTrang99@users.noreply.github.com>
2022-10-29 19:28:43 +0700

    Add files via upload

--- sprites/main.js
+++ sprites/main.js
@@ -1,1 +1,18 @@
-z
+let BGSound;
+
+function preload() {
+    BGSound = loadSound('BG.mp3');
+    setSprites();
+    T_Rex_Animation();
+}
+
+function setup() {
+    createCanvas(gaConfig.screenX, gaConfig.screenY);
+    instializeInSetup(T_rex);
+    BGSound.play();
+    BGSound.loop();
+}
+
+function draw() {
+    game();
+}
